01f25d38df9eef64b7b173a37e6259f5d9952773
Relge0323 <megler@hotmail.com>
2025-12-04 14:02:52 -0600

    Update Appointment.java

--- src/main/java/com/egler/petcare/model/Appointment.java
+++ src/main/java/com/egler/petcare/model/Appointment.java
@@ -9,10 +9,11 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-
-//Appointment represents a scheduled service appointment
-//Links a User, Pet, and Service with a date/time
-//Appointment objects have a status that tracks their lifecycle (PENDING, CONFIRMED, COMPLETED, CANCELLED)
+/**
+ * Appointment represents a scheduled service appointment
+ * Links a User, Pet, and Service with a date/time
+ * Appointment objects have a status that tracks their lifecycle (PENDING, CONFIRMED, COMPLETED, CANCELLED)
+ **/
 @Entity
 @Table(name = "appointments")
 @Data
